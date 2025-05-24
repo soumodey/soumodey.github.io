68ecd84b54b3fb8ba05a12a482e5475732e6eab4
soumodey <daesoumo@gmail.com>
2025-05-24 18:49:31 +0530

    adding style and java script

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scroll for nav links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
